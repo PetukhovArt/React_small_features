1906668eb49b2784cf6e7f350a38aa19808b3c0b
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-02-06 10:17:23 +0500

    feat: microtask2 done

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,24 @@ import './App.css';
 import { Header } from './site/Header';
 import { Body } from './site/Body';
 import { Footer } from './site/Footer';
+import { Task2Map } from './site/Task2Map';
+
+
 
 function App() {
+
+  const topCars = [
+    { manufacturer: 'BMW', model: 'm5cs' },
+    { manufacturer: 'Mercedes', model: 'e63s' },
+    { manufacturer: 'Audi', model: 'rs6' }
+]
+
   return (
     <>
-      <Header titleHeader={'NEW BODY'} />
+      {/* <Header titleHeader={'NEW BODY'} />
       <Body titleBody={'NEW BODY'} />
-      <Footer titleFooter={'NEW BODY'} />
+      <Footer titleFooter={'NEW BODY'} /> */}
+      <Task2Map cars={topCars} />
     </>
   )
 }
